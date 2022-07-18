00a847d38bdb55ba75bdadd80c921621d8a19a30
Derek Parker <parkerderek86@gmail.com>
2022-07-18 10:45:35 -0700

    Rebrake program

--- day-1/cmds/debug/basic/last-character.go
+++ day-1/cmds/debug/basic/last-character.go
@@ -8,12 +8,7 @@ import (
 func main() {
 	fmt.Println("Hello Gophers! Pass me a word and I will print the last character!")
 
-	if len(os.Args) < 2 {
-		fmt.Println("You must supply an argument to this program")
-		os.Exit(1)
-	}
-
 	arg := os.Args[1]
 
-	fmt.Printf("The last character in that word is: %c\n", arg[len(arg)-1])
+	fmt.Printf("The last character in that word is: %c\n", arg[len(arg)])
 }
