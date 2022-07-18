5be0e4fafacff3fbbd4c259718c90f4fb77870ac
Derek Parker <parkerderek86@gmail.com>
2022-07-18 10:57:11 -0700

    Add changes to Go file

--- day-1/cmds/debug/basic/last-character.go
+++ day-1/cmds/debug/basic/last-character.go
@@ -8,6 +8,11 @@ import (
 func main() {
 	fmt.Println("Hello Gophers! Pass me a word and I will print the last character!")
 
+	if len(os.Args) < 2 {
+		fmt.Println("You must supply an argument to this program")
+		os.Exit(1)
+	}
+
 	arg := os.Args[1]
 
 	fmt.Printf("The last character in that word is: %c\n", arg[len(arg)])
