551ad695c70da1db480f00e2877d63f025e74d65
Derek Parker <parkerderek86@gmail.com>
2023-03-15 10:17:26 -0700

    Add test prog and initial commands

---
+++ json-rpc-api/testprog/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+)
+
+type Rectangle struct{}
+
+func (r *Rectangle) Height() int {
+	h, _ := strconv.ParseInt("7", 10, 0)
+	return int(h)
+}
+
+func (r *Rectangle) Width() int {
+	return 6
+}
+
+func (r *Rectangle) Area() int { return r.Height() * r.Width() }
+
+func main() {
+	var r Rectangle
+	runtime.Breakpoint()
+	fmt.Println(r.Area())
+	fmt.Println(r.Area() * 2)
+	fmt.Println(r.Area() * 3)
+}
